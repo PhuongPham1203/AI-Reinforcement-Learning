fb52febf0b6f755488be4df30fa525e7c8fa6f2d
PhuongPham1203 <Phamphuong120398@gmail.com>
2021-09-28 13:39:39 +0700

    update Mounttain car-v0
    Not done

--- MountainCar-v0/MountainCar-v0.py
+++ MountainCar-v0/MountainCar-v0.py
@@ -9,42 +9,36 @@ import pandas as pd
 """
 ! ghi chú về bài toán
 
-    Description:
-        A pole is attached by an un-actuated joint to a cart, which moves along
-        a frictionless track. The pendulum starts upright, and the goal is to
-        prevent it from falling over by increasing and reducing the cart's
-        velocity.
-    Source:
-        This environment corresponds to the version of the cart-pole problem
-        described by Barto, Sutton, and Anderson
+    *Description:
+        The agent (a car) is started at the bottom of a valley. For any given
+        state the agent may choose to accelerate to the left, right or cease
+        any acceleration.
+    *Source:
+        The environment appeared first in Andrew Moore's PhD Thesis (1990).
     *Observation:
-        Type: Box(4)
-        Num     Observation               Min                     Max
-        0       Cart Position             -4.8                    4.8
-        1       Cart Velocity             -Inf                    Inf
-        2       Pole Angle                -0.418 rad (-24 deg)    0.418 rad (24 deg)
-        3       Pole Angular Velocity     -Inf                    Inf
+        Type: Box(2)
+        Num    Observation               Min            Max
+        0      Car Position              -1.2           0.6
+        1      Car Velocity              -0.07          0.07
     *Actions:
-        Type: Discrete(2)
-        Num   Action
-        0     Push cart to the left
-        1     Push cart to the right
-        Note: The amount the velocity that is reduced or increased is not
-        fixed; it depends on the angle the pole is pointing. This is because
-        the center of gravity of the pole increases the amount of energy needed
-        to move the cart underneath it
+        Type: Discrete(3)
+        Num    Action
+        0      Accelerate to the Left
+        1      Don't accelerate
+        2      Accelerate to the Right
+        Note: This does not affect the amount of velocity affected by the
+        gravitational pull acting on the car.
     *Reward:
-        Reward is 1 for every step taken, including the termination step
+         Reward of 0 is awarded if the agent reached the flag (position = 0.5)
+         on top of the mountain.
+         Reward of -1 is awarded if the position of the agent is less than 0.5.
     *Starting State:
-        All observations are assigned a uniform random value in [-0.05..0.05]
+         The position of the car is assigned a uniform random value in
+         [-0.6 , -0.4].
+         The starting velocity of the car is always assigned to 0.
     *Episode Termination:
-        Pole Angle is more than 12 degrees.
-        Cart Position is more than 2.4 (center of the cart reaches the edge of
-        the display).
-        Episode length is greater than 200.
-        Solved Requirements:
-        Considered solved when the average return is greater than or equal to
-        195.0 over 100 consecutive trials.
+         The car position is more than 0.5
+         Episode length is greater than 200
 
 
 """
@@ -58,21 +52,19 @@ def load_weight(dirfile):
 def create_Qtable(state_space, action_space, bins_size = 30):
     
     bins = np.array([
-        np.linspace(-4.8,4.8, num=bins_size), # Cart Position : -4.8 -> 4.8
-        np.linspace(-10,10, num=bins_size), # Cart Velocity : -Inf -> Inf
-        np.linspace(-0.418,0.418, num=bins_size), # Pole Angle :-0.418 rad (-24 deg) -> 0.418 rad (24 deg)
-        np.linspace(-10,10, num=bins_size), # Pole Angular Velocity : -Inf -> Inf
-    
-    ])
+        np.linspace(-1.2,0.6, num=bins_size), # Car Position : -1.2 -> 0.6
+        np.linspace(-0.07,0.07, num=bins_size), # Car Velocity : -0.07 -> 0.07
+        
+       ])
     #print(bins.shape)
 
     q_table = np.random.uniform(low=-1,high=1,size=([bins_size] * state_space + [action_space]))
-    # size = [30,30,30,30,2]
+    # size = [30,30,3]
     
     return q_table, bins
 
 def discrete(state, bins): # find index of state in linespace
-    #? index = discrete([4.8,10,0.418,10],bins=bins) # how to use
+    #? index = discrete([-1.2,-0.07],bins=bins) # how to use
 
     index = []
     for i in range(len(state)): index.append(np.digitize(state[i],bins[i]) - 1)
@@ -84,23 +76,28 @@ def get_next_action(q_table,state_current,bins,epsilon):
     #print('index in table ',index,'value',q_table[index[0]][index[1]][index[2]][index[3]])
     
     if(np.random.random() <= epsilon): # chose from table
-        action = np.argmax( q_table[index[0]][index[1]][index[2]][index[3]] )
-        q_value = q_table[index[0]][index[1]][index[2]][index[3]][action]
+        action = np.argmax( q_table[index[0]][index[1]] )
+        q_value = q_table[index[0]][index[1]][action]
         return action, q_value, index
     else: # chose random
         action = random.randint(0,1)
-        q_value = q_table[index[0]][index[1]][index[2]][index[3]][action]
+        q_value = q_table[index[0]][index[1]][action]
         return action, q_value, index
 
 def run_game_to_create_weight(env,k_game,episode, q_table,bins, epsilon,discount_factor,learning_rate):
     
-    
+    max_pos_L = 2        
+    max_pos_R = -2
+
+    max_vec_L = 1
+    max_vec_R = -1
 
     for k in range(k_game):
         #? observation = state_current
         observation = env.reset()
         sum_reward = 0
         #print("k_game = ", k, end=' -> ')
+        
         
 
         for e in range(episode):
@@ -112,22 +109,44 @@ def run_game_to_create_weight(env,k_game,episode, q_table,bins, epsilon,discount
             
             # perform action
             observation, reward, done, info = env.step(action_should_do)
-            
+            if(reward == -1):
+                r = 0
+                if( observation[0]>max_pos_R):
+                    max_pos_R = observation[0]
+                    r += 1
+                if( observation[0]<max_pos_L):
+                    max_pos_L = observation[0]
+                    r += 2
+                
+                if( observation[1]>max_vec_R):
+                    max_vec_R = observation[1]
+                    r += 1
+                if( observation[1]<max_vec_L):
+                    max_vec_L = observation[1]
+                    r += 2
+
+                if(r > 0):
+    	            reward = r
+
+            elif(reward == 0):
+                reward = 10
+                
+                
             # measure reward 
-            sum_reward += 1
+            sum_reward += reward
             
             index_new = discrete(observation,bins)
-            temp = reward + (discount_factor * np.max(q_table[index_new[0]][index_new[1]][index_new[2]][index_new[3]])) - q_value 
+            temp = reward + (discount_factor * np.max(q_table[index_new[0]][index_new[1]])) - q_value 
             new_q_value = q_value + (learning_rate * temp)
             
             # update Q_table
-            q_table[index[0]][index[1]][index[2]][index[3]][action_should_do] = new_q_value
+            q_table[index[0]][index[1]][action_should_do] = new_q_value
 
             if done:
-                #print("Episode length = ",sum_reward)
+                print("Game ",k," length reward = ",sum_reward)
                 break
 
-
+        print(max_pos_L,max_pos_R,max_vec_L,max_vec_R)
         
 
 
@@ -162,51 +181,12 @@ def run_game(env,k_game,episode, q_table,bins):
     env.close()
     return arr_reward
 
-
-
-def main_v001():
-    # *dir file weight 
-    dirfile = 'weight\CartPole-v0_Qtable_v001.npy'
-
-    # *rate
-    epsilon = 0.9
-    discount_factor = 0.9
-    learning_rate = 0.9
-
-    # *create env
-    env = gym.make('MountainCar-v0')
-
-    # * action, reward, q_table(s,a)
-    action = ['left', 'right']
-    reward = 1
-
-    state_space = 4
-    action_space = 2
-    bins_size = 30 # state can infinity, so we collap them to bins
-
-    #q_table,bins = create_Qtable(state_space,action_space,bins_size)
-    #print("q_table",q_table.shape,",bins",bins.shape)
-
-    env.reset()
-    for _ in range(200):
-        env.render()
-        
-
-        action = env.action_space.sample()
-        observation, reward, done, info = env.step(action)
-        print(observation,reward)
-        if(done):
-            break
-
-    env.close()
-
-    
-    '''
+def calculate_weight_and_save(env,q_table,bins,epsilon,discount_factor,learning_rate, dirfile):
     # TODO : calculate weight and save
-    k_game = 300
+    k_game = 250
     print('\nrun n game ')
     q_table = run_game_to_create_weight(env=env,
-                                        k_game=1000,
+                                        k_game=k_game,
                                         episode=200,
                                         q_table=q_table,
                                         bins=bins,
@@ -217,15 +197,13 @@ def main_v001():
     
     save_weight(dirfile,q_table)
     print('save weight to ',dirfile)
-    '''
 
-    '''
-    # TODO : load weight and run
+def load_weight_and_run(env,q_table,bins, dirfile):
     q_table = load_weight(dirfile)
     print('q_table ',q_table.shape)
 
     # k_game>100 => if 100 game have average episode ~~ 195  
-    k_game = 10
+    k_game = 3
     arr_reward = run_game(env=env,
                             k_game=k_game,
                             episode=200,
@@ -233,15 +211,58 @@ def main_v001():
                             bins=bins)
     arr_reward = np.array(arr_reward)
     print('Average ',k_game,'k_game = ',np.average( arr_reward ) )
+def test(env):
+    env.reset()
+    for _ in range(200):
+        env.render()
+        action = env.action_space.sample()
+        observation, reward, done, info = env.step(action)
+        print(observation,reward)
+        if(done):
+            break
+    env.close()
 
-    arr_reward_Solved = np.delete(arr_reward,np.where(arr_reward<195))
-    print('Average ',arr_reward_Solved.shape[0],'k_game_Solved = ',np.average( arr_reward_Solved ) )
-    '''
-    #*Average  300 k_game =  144.57666666666665
-    #*Average  78 k_game_Solved =  199.66666666666666
-    #*Average  2000 k_game =  140.656
-    #*Average  501 k_game_Solved =  199.71656686626747
+def main_v001():
+    # *dir file weight 
+    dirfile = 'weight\MountainCar-v0_Qtable_v001.npy'
 
+    # *rate
+    epsilon = 0.8 #0.92 #0.80
+    discount_factor = 0.84 #0.92 #0.84
+    learning_rate = 0.84 #0.92 #0.84
+
+    # *create env
+    env = gym.make('MountainCar-v0')
+
+    # * action, reward, q_table(s,a)
+    action = ['left', 'right']
+    reward = 1
+
+    state_space = 2
+    action_space = 3
+    bins_size = 30 # state can infinity, so we collap them to bins
+
+    q_table,bins = create_Qtable(state_space,action_space,bins_size)
+    print("q_table",q_table.shape,",bins",bins.shape)
+
+    
+    # TODO : Test
+    #test(env)
+    
+    
+    # TODO : calculate weight and save
+    calculate_weight_and_save(env,q_table,bins,epsilon,discount_factor,learning_rate, dirfile)
+
+    
+    # TODO : load weight and run
+    load_weight_and_run(env,q_table,bins, dirfile)
+
+    
+
+    #arr_reward_Solved = np.delete(arr_reward,np.where(arr_reward<195))
+    #print('Average ',arr_reward_Solved.shape[0],'k_game_Solved = ',np.average( arr_reward_Solved ) )
+    
+    
 if(__name__ == '__main__'):
     
 
